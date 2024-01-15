441970bfad5186a52baebff79bb8f9fb4c486041
Tran Vu Binh <tranb4010@gmail.com>
2024-01-15 14:37:15 +0700

    first commit, setup

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys, pygame
+from settings import *
+from tiles import Tile
+from level import Level
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+level = Level(level_map, screen)
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill("black")
+    level.run()
+
+
+    pygame.display.flip()
+    clock.tick(60)
