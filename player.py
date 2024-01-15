3338fbc617128fd921a5ba98629818528ba28ca2
Tran Vu Binh <tranb4010@gmail.com>
2024-01-15 14:47:56 +0700

    update

--- player.py
+++ player.py
@@ -20,9 +20,15 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 1
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+        elif keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
         else:
             self.direction.x = 0
+            self.direction.y = 0
 
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
